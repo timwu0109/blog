527ec613037d6d81354b8e148132e1433d2c7817
TimWu <mr.oldfive@gmail.com>
2023-07-06 23:11:13 +0800

    make user and blog controller/ setting the route /db mysql change to sqlite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::resource('/user'  ,  UserController::class);
+
+Route::prefix('user')->group(function () {
+    Route::resource('blog', BlogController::class);
 });
