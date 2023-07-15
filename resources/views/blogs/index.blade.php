ceb96428b74e465a0c861ca14132144444dcd9ea
TimWu <mr.oldfive@gmail.com>
2023-07-15 12:28:31 +0800

    create blog controller

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -3,4 +3,12 @@
 @section('main')
     <h1> blog > index</h1>
     <a href="{{ route('blog.create')}}"> 新增文章</a>
+
+    @foreach ($blogs as $blog)
+        <div class='bg-red-300 mt-6'>
+            <h3 class='border border-bottom border-stone-700'>{{ $blog->title }}</h3>
+            <p>{{ $blog->content }}</p>
+            <span>作者：{{ $blog->user->name }} 文章建立時間：{{ $blog->created_at }}</span>
+        </div>
+    @endforeach
 @endsection
