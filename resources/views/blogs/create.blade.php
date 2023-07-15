cbe2baa5ae1d151f2bb845b21364aa3b78a0065e
TimWu <mr.oldfive@gmail.com>
2023-07-15 12:29:30 +0800

    create blog model and migration / add create view

--- resources/views/blogs/create.blade.php
+++ resources/views/blogs/create.blade.php
@@ -2,13 +2,19 @@
 
 @section('main')
     <h1> blog > create</h1>
-    <form action="{{ route('blog.store') }}" method="post">
+    <form action="{{ route('blog.store') }}" method="post" class="">
         @csrf
         <div>
-            <label for=""></label>
-            <input type="text" name=>
+            <label for='title'> 標題：</label>
+            <input type="text" name='title' class="border border-pink-300">
+        </div>
+        <div>
+            <label for='content'> 內文：</label>
+            <textarea name="content" cols="30" rows="10" class="border border-pink-300"></textarea>
         </div>
     
-    
+        <div>
+            <button type='submit'>建立文章</button>
+        </div>
     </form>
 @endsection
